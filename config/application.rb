257455ba906270658efd9d72371d5a99dd058a13
Nandhini Raj <nandhinirajp@gmail.com>
2025-04-17 21:41:35 +0530

    pipeline error

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,7 @@
 require_relative "boot"
 
-require "rails"
-require "active_model/railtie"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
-require "active_job/railtie"
-require "rails/test_unit/railtie"
+require "rails/all"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
