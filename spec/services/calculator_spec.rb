33d0d83c3f37a387d67430d87f65f55556a4e410
Nandhini Raj <nandhinirajp@gmail.com>
2025-04-17 21:03:30 +0530

    Update Gemfile and test cases to reflect changes in dependencies

--- spec/services/calculator_spec.rb
+++ spec/services/calculator_spec.rb
@@ -22,5 +22,18 @@ RSpec.describe Calculator, type: :service do
             expect(Calculator.add("1\n2\n3")).to eq(6)
         end
     end
+
+    context "when the input contains a custom delimiter" do
+        it "returns the sum of numbers using the custom delimiter" do
+          expect(Calculator.add("//;\n1;2;3")).to eq(6)
+        end
+      end
+  
+      context "when the input contains negative numbers" do
+        it "raises an error for negative numbers" do
+          expect { Calculator.add("1,-2,3") }.to raise_error("negative numbers not allowed: -2")
+        end
+      end
+      
   end
 end
